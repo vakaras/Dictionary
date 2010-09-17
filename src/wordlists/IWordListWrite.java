9fde95936273ba6fce1bea7e05776b02c4c95cab
Vytautas Astrauskas <vastrauskas@gmail.com>
2010-09-17 11:18:33 +0300

    Added IWordListWrite interface.

---
+++ src/wordlists/IWordListWrite.java
@@ -0,0 +1,12 @@
+/**
+ * Interface defining word list, which could be written to file.
+ */
+public interface IWordListWrite {
+
+  /**
+   * Save word list to file.
+   * @param filename – path to file to which save the dictionary.
+   */
+  void save(String filename);
+
+  }
