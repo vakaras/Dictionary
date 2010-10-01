81f0a9804928c48f6ef621e80ff29698c0bc1dca
Vytautas Astrauskas <vastrauskas@gmail.com>
2010-10-01 17:05:53 +0300

    Added concurent searches test for DWAFile.

--- src/tests/TestDWAFile.java
+++ src/tests/TestDWAFile.java
@@ -12,6 +12,21 @@ public class TestDWAFile extends TestCase {
 
   private wordlists.DWAFile wordList;
 
+  private void assertEquals(LinkedList<Word> expected, 
+      LinkedList<Word> actual) {
+
+    super.assertEquals(expected.size(), actual.size());
+
+    for (int i = 0; i < expected.size(); i++) {
+      Word expectedWord = expected.get(i);
+      Word actualWord = actual.get(i);
+      super.assertEquals(expectedWord.getWord(), actualWord.getWord());
+      super.assertEquals(expectedWord.getDescription(), 
+          actualWord.getDescription());
+      }
+    
+    }
+
   public void setUp() {
     wordList = new wordlists.DWAFile();
     }
@@ -121,13 +136,99 @@ public class TestDWAFile extends TestCase {
     LinkedList<Word> result = wordList.search("bbai", 5);
 
     super.assertEquals(4, result.size());
+
+    Word i = result.get(0);
+    super.assertEquals("bbai", i.getWord());
+    super.assertEquals("Description 20.", i.getDescription());
+
+    i = result.get(1);
+    super.assertEquals("caa", i.getWord());
+    super.assertEquals(
+        "Description 21.  Testing if shorter is before longer.", 
+        i.getDescription());
+
+    i = result.get(2);
+    super.assertEquals("caaa", i.getWord());
+    super.assertEquals("Description 22.", i.getDescription());
+
+    i = result.get(3);
+    super.assertEquals("caab", i.getWord());
+    super.assertEquals("Description 23.", i.getDescription());
+
     }
 
-  public void testConcurentSearch() throws Exception {
+  class RunnableSearch implements Runnable {
+
+    private wordlists.WordList wordList = null;
+    private TestCase test = null;
+    private String request = null;
+    private int count = 0;
+    private LinkedList<Word> result = null;
+    private Throwable exception = null;
+
+    public RunnableSearch(wordlists.WordList wordList, TestCase test,
+        String request, int count) {
+      this.wordList = wordList;
+      this.test = test;
+      this.request = request;
+      this.count = count;
+      }
+    
+    public LinkedList<Word> getResult() {
+      return this.result;
+      }
+
+    public Throwable getException() {
+      return this.exception;
+      }
+    
+    public void run() {
+      try {
+        //System.out.println("Started: \"" + this.request + "\"");
+        this.result = wordList.search(this.request, this.count);
+        //System.out.println("Finished: \"" + this.request + "\"");
+        }
+      catch (Throwable e) {
+        this.exception = e;
+        }
+      }
+    
+    }
+
+  public void testConcurentSearch() throws Throwable {
 
     wordList.load("tests/test.dwa");
 
-    LinkedList<Word> result = wordList.search("ąa", 100);
+    LinkedList<Word> expectedResultLong = wordList.search("", 100);
+    LinkedList<Word> expectedResultShort = wordList.search("aa", 2);
+
+    RunnableSearch r1 = new RunnableSearch(wordList, this, "", 100);
+    RunnableSearch r2 = new RunnableSearch(wordList, this, "aa", 2);
+
+    Thread thread1 = new Thread(r1);
+    Thread thread2 = new Thread(r2);
+
+    thread1.start();
+    thread2.start();
+
+    thread1.join();
+    thread2.join();
+
+    Throwable e = r1.getException();
+    if (e != null) {
+      throw e;
+      }
+    e = r2.getException();
+    if (e != null) {
+      throw e;
+      }
+
+    LinkedList<Word> concurentResultLong = r1.getResult();
+    LinkedList<Word> concurentResultShort = r2.getResult();
+
+    this.assertEquals(expectedResultLong, concurentResultLong);
+    this.assertEquals(expectedResultShort, concurentResultShort);
+
     }
 
   }
